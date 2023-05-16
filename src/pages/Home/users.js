8bd67c3ff6babc1e836b567810b382e771ba8e91
rsibarra <rsibarra94@gmail.com>
2023-05-16 13:42:52 -0400

    actualizacion de user

--- src/pages/Home/users.js
+++ src/pages/Home/users.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import Entrenadores from "./components/Entrenadores";
+import Boton from "./components/Boton";
+import Modal from "./components/ModalUser";
+import "./components/Boton";
 
 const Users = () =>{
+  const [showModal, setShowModal] = useState(false);
+  const [entrenadores, setEntrenadores] = useState([
+    
+  ]);
+
+  const handleAgregarClick = () => {
+    setShowModal(true);
+  };
+
+  const handleAddTrainer = (trainer) => {
+    setEntrenadores([...entrenadores, trainer]);
+  };
     return(
       <>
-      <h1>Clientes</h1>
-      <h2>Saludo</h2>
-      </>
-      
+        <div className="boton-container">
+          <Boton palabra="Agregar" onClick={handleAgregarClick} />
+        </div>
+        {entrenadores.map((trainer, index) => (
+        <Entrenadores key={index} photo={trainer.photo} name={trainer.name} modalidad={trainer.modalidad} />
+      ))}
+        {showModal && (
+          <Modal onClose={() => setShowModal(false)} onAddTrainer={handleAddTrainer} />
+        )}
+      </>  
     );
 }
 
