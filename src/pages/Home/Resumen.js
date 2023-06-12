270c6c6f2b1bbf7ff0fede26f39a818a346dbbdd
CesarXaio <cesararce@fiuni.edu.py.com>
2023-06-12 10:40:01 -0400

    cambio Cesar

--- src/pages/Home/Resumen.js
+++ src/pages/Home/Resumen.js
@@ -9,49 +9,7 @@ import 'chartjs-adapter-moment';
 const Resumen = () => {
   const montoCaja = 1000; // Cantidad de dinero en la caja
   const montoPlata = 500; // Cantidad de dinero en la sección "Plata"
-  const dataInforme1 = {
-    labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
-    datasets: [
-      {
-        label: 'Informe 1',
-        data: [50, 75, 60, 80, 70, 90],
-        fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1,
-      },
-    ],
-  };
   
-  const optionsInforme1 = {
-    scales: {
-      y: {
-        type: 'linear',
-        beginAtZero: true,
-      },
-    },
-  };
-  
-  const dataInforme2 = {
-    labels: ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4'],
-    datasets: [
-      {
-        label: 'Informe 2',
-        data: [100, 150, 120, 180],
-        fill: false,
-        borderColor: 'rgb(255, 99, 132)',
-        tension: 0.1,
-      },
-    ],
-  };
-  
-  const optionsInforme2 = {
-    scales: {
-      y: {
-        type: 'linear',
-        beginAtZero: true,
-      },
-    },
-  };
   
   return (
     <div className="resumen-container">
@@ -72,17 +30,7 @@ const Resumen = () => {
           <p className="Cierre-Caja">Hora de cierre de caja 18:00 </p>
         </div>
       </div>
-      <div className="row2">
-        <div className="Informe1">
-            <Line className="Inf-1" data={dataInforme1} options={optionsInforme1} />
-            <p className="Informe-Semanal">Informe semanal</p>
-        </div>
-        <div className="Informe2">
-            
-            <Line className="Inf-2" data={dataInforme2} options={optionsInforme2} />
-            <p className="Informe-Semana2">Informe semanal</p>
-        </div>
-      </div>
+      
     </div>
   );
 };
--- src/pages/Home/components/modalUsers/Membresia.js
+++ src/pages/Home/components/modalUsers/Membresia.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Mensaje from "../../../../Confirmacion/Mensaje";
 import "./Membresia.css";
 import axios from "axios";
