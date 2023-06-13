522be1491a9d5ed931e2379f4e8825fba04f6169
rsibarra <rsibarra94@gmail.com>
2023-06-13 19:24:20 -0400

    retorno de detalles del user

--- src/pages/Home/components/modalUsers/DetallesUser.js
+++ src/pages/Home/components/modalUsers/DetallesUser.js
@@ -1,46 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "././DetallesUser.css";
 
-const DetallesUser = ({usuario, onClickAvance, onClose, onAddTrainer })=> {
+const DetallesUser = ({ usuario, onClickAvance, onClose, onAddTrainer }) => {
+
+
+  const [medidas, setMedidas] = useState([]);
+  useEffect(() => {
+    const getMedidas = async () => {
+      try {
+        const response = await fetch("https://localhost:44373/api/caja");
+        const data = await response.json();
+        const medidas = {
+          fecha: data.fecha,
+          hora_inicio: data.hora_inicio,
+          monto_inicial: data.monto_inicial,
+          monto_actual: data.monto_actual,
+          esAbierta: data.abierta
+        }
+        setMedidas(data)
+      } catch (error) {
+        console.error("Error al obtener los Productos:", error);
+      }
+    };
+
+    getMedidas(); // Llamar a la función para obtener los Productos al cargar el componente
+  }, []); // El segundo argumento es un arreglo vacío, esto indica que solo se ejecutará una vez al cargar el componente
+
+
+
+  const medidasDiv = ((m) => {
+    return (
+      <div>
+        <div className="elementoTarjeta separacion">{m.medidas.altura}</div>
+        <div className="elementoTarjeta separacion">{m.medidas.peso}</div>
+        <div className="elementoTarjeta separacion">{m.medidas.cintura}</div>
+        <div className="elementoTarjeta separacion">{m.medidas.pecho}</div>
+        <div className="elementoTarjeta separacion">{m.medidas.cadera}</div>
+      </div>
+    )
+  })
 
   return (
     <>
       <div className="modal-overlay">
         <div className="modalUserSimpleDetalle">
           <div className="modal-body-User">
-              <div className="contenedorDetalle">
-                <h3 className="tituloDetalle">fecha</h3>
-                <h3 className="tituloDetalle">altura</h3>
-                <h3 className="tituloDetalle">peso</h3>
-                <h3 className="tituloDetalle">cintura</h3>
-                <h3 className="tituloDetalle">pecho</h3>
-                <h3 className="tituloDetalle">cadera</h3>
-              </div>
-              <div className="tarjetasDetalle">
-                <div className="elementosDetalles">{usuario.altura}</div>
-                <div className="elementosDetalles">{usuario.altura}</div>
-                <div className="elementosDetalles">{usuario.peso}</div>
-                <div className="elementosDetalles">{usuario.cintura}</div>
-                <div className="elementosDetalles">{usuario.pecho}</div>
-                <div className="elementosDetalles">{usuario.cadera}</div> 
-              </div>
-              <div className="tarjetasDetalle">
-                <div className="elementosDetalles">{usuario.altura}</div>
-                <div className="elementosDetalles">{usuario.altura}</div>
-                <div className="elementosDetalles">{usuario.peso}</div>
-                <div className="elementosDetalles">{usuario.cintura}</div>
-                <div className="elementosDetalles">{usuario.pecho}</div>
-                <div className="elementosDetalles">{usuario.cadera}</div> 
-              </div>
-              <div className="tarjetasDetalle">
-                <div className="elementosDetalles">{usuario.altura}</div>
-                <div className="elementosDetalles">{usuario.altura}</div>
-                <div className="elementosDetalles">{usuario.peso}</div>
-                <div className="elementosDetalles">{usuario.cintura}</div>
-                <div className="elementosDetalles">{usuario.pecho}</div>
-                <div className="elementosDetalles">{usuario.cadera}</div> 
-              </div>
-              <div className="button-container">
+            {medidas.map(m => (
+              medidasDiv(m)
+            )
+            )}
+            <div className="button-container">
               <a id="boton-off" onClick={() => {onClickAvance(-1);}}>
                 cancelar
               </a>
