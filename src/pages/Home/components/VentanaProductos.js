819f800b917cb924530d2a7dbf5813642598fb95
CesarXaio <cesararce@fiuni.edu.py.com>
2023-06-16 09:02:16 -0400

    Estilos a mostrar productos

--- src/pages/Home/components/VentanaProductos.js
+++ src/pages/Home/components/VentanaProductos.js
@@ -1,21 +1,29 @@
 import React from "react";
-import './VentanaProductos.css'
+import "./VentanaProductos.css";
 
 const VentanaProductos = ({ productos, onClose }) => {
   return (
     <div className="ventana-productos">
       <div className="ventana-productos-contenido">
         <h2>Productos en Stock</h2>
-        <ul>
-          {productos.map((producto) => (
-            <li key={producto.codigo_barra}>
-              <span>{producto.descripcion}</span>
-              <span>{producto.precio}</span>
-              <span>{producto.cantidadDB} unidades</span>
-              {/* Agrega más información del producto si deseas */}
-            </li>
-          ))}
-        </ul>
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Precio</th>
+              <th>Cantidad</th>
+            </tr>
+          </thead>
+          <tbody>
+            {productos.map((producto) => (
+              <tr key={producto.codigo_barra}>
+                <td>{producto.descripcion}</td>
+                <td>{producto.precio}</td>
+                <td>{producto.cantidadDB} unidades</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <button className="cerrar-ventana" onClick={onClose}>
           Cerrar
         </button>
